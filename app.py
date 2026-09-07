287753bc575f148fa77db678b1fff2149c1f2bad
oscarschmidtrodriguez-lab <oscar.schmidt.rodriguez@gmail.com>
2026-09-07 20:46:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ if __name__ == "__main__":
     from scheduler import init_scheduler
     init_scheduler(app)
 
-        port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
